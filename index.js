5e5030eb7cb2c03940d137c52a49d68e67658595
Brandon Cacsire <bc@BCs-MacBook-Air.lehman.edu>
2023-02-08 17:15:28 -0500

    final materials

---
+++ index.js
@@ -0,0 +1,8 @@
+// Shout out w3schools and ChatGPT!
+
+document.addEventListener("DOMContentLoaded", function() {
+    var homeButton = document.querySelector("#gitHubButton");
+    homeButton.addEventListener("click", function() {
+     alert("You are going to GitHub.");
+    });
+   });
